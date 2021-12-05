4e0cae48c1b5bc0dc9abdb1d56756116d63f74bd
Alex C. Viana <alexcostaviana@gmail.com>
2021-12-04 23:00:54 -0600

    Day 1 Part 2

--- advent_of_code_2021/day_1.py
+++ advent_of_code_2021/day_1.py
@@ -10,12 +10,23 @@ def parse_data(data: List[int]) -> List[bool]:
     return [True if data[i] > data[i - 1] else False for i in range(1, len(data))]
 
 
+def window_sum(data: List[int]) -> List[int]:
+    return [sum(data[i : i + 3]) for i in range(0, len(data) - 2)]
+
+
 def main() -> None:
     data = load_data()
+    print("Part 1")
     print(f"Length of data: {len(data)}")
     result = parse_data(data)
     print(f"Length of result: {len(result)}")
     print(f"Solution: {sum(result)}")
+
+    print("Part 2")
+    print(f"Length of data: {len(data)}")
+    windowed_data = window_sum(data)
+    print(f"Length of windowed data: {len(windowed_data)}")
+    print(f"Solution: {sum(parse_data(windowed_data))}")
 
 
 if __name__ == "__main__":
--- tests/day_1_test.py
+++ tests/day_1_test.py
@@ -1,4 +1,4 @@
-from advent_of_code_2021.day_1 import parse_data
+from advent_of_code_2021.day_1 import parse_data, window_sum
 
 TEST_DATA = """199
 200
@@ -12,11 +12,19 @@ TEST_DATA = """199
 263"""
 TEST_DATA = [int(item) for item in TEST_DATA.split("\n")]
 
-EXPECTED_RESULT = [True, True, True, False, True, True, True, False, True]
+EXPECTED_TEST_RESULT = [True, True, True, False, True, True, True, False, True]
 
 
 def test_parse_data():
     result = parse_data(TEST_DATA)
-    assert result == EXPECTED_RESULT
+    assert result == EXPECTED_TEST_RESULT
     assert len(result) == len(TEST_DATA) - 1
     assert sum(result) == 7
+
+
+TEST_WINDOW_SUM_RESULT = [607, 618, 618, 617, 647, 716, 769, 792]
+
+
+def test_window_data():
+    result = window_sum(TEST_DATA)
+    assert result == TEST_WINDOW_SUM_RESULT
