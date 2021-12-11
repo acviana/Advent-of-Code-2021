60744c4f7f2cb48433bba44045e2cdbb74ffdc95
Alex C. Viana <alexcostaviana@gmail.com>
2021-12-11 11:53:52 -0600

    Day 2 Part 2: Solution works but type checking fails.

--- advent_of_code_2021/day_2.py
+++ advent_of_code_2021/day_2.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 
 def load_data() -> List[List[str]]:
@@ -6,28 +6,76 @@ def load_data() -> List[List[str]]:
         return [item.strip().split(" ") for item in f.readlines()]
 
 
-def translate_instruction(instruction: List[str]) -> Tuple[int, int]:
-    if instruction[0] == "up":
-        return (0, -1 * int(instruction[1]))
-    elif instruction[0] == "down":
-        return (0, int(instruction[1]))
-    elif instruction[0] == "forward":
-        return (int(instruction[1]), 0)
+def translate_instruction_part_1(input_instruction: List[str]) -> Dict:
+    instruction = {
+        "direction": input_instruction[0],
+        "magnitude": int(input_instruction[1]),
+    }
+    if instruction["direction"] == "up":
+        return {"position_change": 0, "depth_change": -1 * instruction["magnitude"]}
+    elif instruction["direction"] == "down":
+        return {"position_change": 0, "depth_change": instruction["magnitude"]}
+    elif instruction["direction"] == "forward":
+        return {"position_change": instruction["magnitude"], "depth_change": 0}
+    else:
+        assert False
+
+
+def translate_instruction_part_2(input_instruction: List[str]) -> Dict:
+    instruction = {
+        "direction": input_instruction[0],
+        "magnitude": int(input_instruction[1]),
+    }
+    if instruction["direction"] == "up":
+        return {
+            "position_change": 0,
+            "depth_change": 0,
+            "aim_change": -1 * instruction["magnitude"],
+        }
+    elif instruction["direction"] == "down":
+        return {
+            "position_change": 0,
+            "depth_change": 0,
+            "aim_change": instruction["magnitude"],
+        }
+    elif instruction["direction"] == "forward":
+        return {
+            "position_change": instruction["magnitude"],
+            "depth_change_multiple": instruction["magnitude"],
+            "aim_change": 0,
+        }
     else:
         assert False
 
 
 def parse_data(data: List[List[str]]) -> Tuple[int, int]:
-    instruction_list = [translate_instruction(item) for item in data]
+    instruction_list = [translate_instruction_part_1(item) for item in data]
     return (
-        sum([item[0] for item in instruction_list]),
-        sum([item[1] for item in instruction_list]),
+        sum([item["position_change"] for item in instruction_list]),
+        sum([item["depth_change"] for item in instruction_list]),
     )
+
+
+def parse_data_2(data: List[List[str]]) -> Tuple[int, int]:
+    instruction_list = [translate_instruction_part_2(item) for item in data]
+    coordinate_dict = {"position": 0, "depth": 0, "aim": 0}
+    for instruction in instruction_list:
+        if instruction["aim_change"] != 0:
+            coordinate_dict["aim"] += instruction["aim_change"]
+        else:
+            coordinate_dict["position"] += instruction["position_change"]
+            coordinate_dict["depth"] += (
+                instruction["depth_change_multiple"] * coordinate_dict["aim"]
+            )
+    return (coordinate_dict["position"], coordinate_dict["depth"])
 
 
 def main() -> None:
     data = load_data()
     position = parse_data(data)
+    print(f"Position is {position}")
+    print(f"Answer in {position[0] * position[1]}")
+    position = parse_data_2(data)
     print(f"Position is {position}")
     print(f"Answer in {position[0] * position[1]}")
 
--- tests/day_2_test.py
+++ tests/day_2_test.py
@@ -1,4 +1,4 @@
-from advent_of_code_2021.day_2 import parse_data
+from advent_of_code_2021.day_2 import parse_data, parse_data_2
 
 TEST_DATA = """forward 5
 down 5
@@ -11,3 +11,7 @@ TEST_DATA = [item.strip().split(" ") for item in TEST_DATA.split("\n")]
 
 def test_parse_data():
     assert (15, 10) == parse_data(TEST_DATA)
+
+
+def test_parse_data_2():
+    assert (15, 60) == parse_data_2(TEST_DATA)
