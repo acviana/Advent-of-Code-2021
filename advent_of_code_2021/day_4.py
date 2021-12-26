112870c65b11a27a5a58efd7e5b6eb656408b70f
Alex C. Viana <alexcostaviana@gmail.com>
2021-12-26 09:33:07 -0600

    Day 4 Part 1 (need to fix type checking)

---
+++ advent_of_code_2021/day_4.py
@@ -0,0 +1,105 @@
+from typing import List
+
+
+def load_data() -> List[str]:
+    with open("inputs/day_4_input.txt") as f:
+        return [item.strip() for item in f.readlines()]
+
+
+def parse_data(data: List[str]) -> dict:
+    output = {
+        "numbers": [],
+        "boards": [],
+    }
+
+    output["numbers"] = [int(item) for item in data[0].split(",")]
+
+    temp_board = []
+    for row in data[2:]:
+        if row == "":
+            output["boards"] += [temp_board]
+            temp_board = []
+        else:
+            temp_board += [[int(item) for item in row.split()]]
+    output["boards"] += [temp_board]
+
+    return output
+
+
+class Board:
+    def __init__(self, input):
+        self._input = input
+        self._load_board()
+
+    def _load_board(self):
+        board = {}
+        for row_index in range(0, 5):
+            for column_index in range(0, 5):
+                board[(column_index, row_index)] = {
+                    "number": self._input[column_index][row_index],
+                    "picked": False,
+                }
+        self.board = board
+
+    def _is_winner(self) -> bool:
+        for column_index in range(0, 5):
+            column_sum = 0
+            for row_index in range(0, 5):
+                column_sum += self.board[(column_index, row_index)]["picked"]
+            if column_sum == 5:
+                return True
+
+        for row_index in range(0, 5):
+            row_sum = 0
+            for column_index in range(0, 5):
+                row_sum += self.board[(column_index, row_index)]["picked"]
+            if row_sum == 5:
+                return True
+
+        return False
+
+    @property
+    def is_winner(self) -> bool:
+        return self._is_winner()
+
+    def _unmarked_sum(self) -> int:
+        unmarked_sum = 0
+        for row_index in range(0, 5):
+            for column_index in range(0, 5):
+                if self.board[(column_index, row_index)]["picked"] is False:
+                    unmarked_sum += self.board[(column_index, row_index)]["number"]
+        return unmarked_sum
+
+    @property
+    def unmarked_sum(self) -> int:
+        return self._unmarked_sum()
+
+    def pick_number(self, number: int) -> None:
+        for row_index in range(0, 5):
+            for column_index in range(0, 5):
+                if self.board[(column_index, row_index)]["number"] == number:
+                    self.board[(column_index, row_index)]["picked"] = True
+
+
+def run_game(parsed_data: dict) -> dict:
+    boards = [Board(item) for item in parsed_data["boards"]]
+    for number in parsed_data["numbers"]:
+        for board in boards:
+            board.pick_number(number)
+            if board.is_winner:
+                return {"number": number, "unmarked_sum": board.unmarked_sum}
+    assert False
+
+
+def main() -> None:
+    data = load_data()
+    parsed_data = parse_data(data)
+    game_winner = run_game(parsed_data)
+    answer = game_winner["number"] * game_winner["unmarked_sum"]
+    print(
+        f'Answer is {game_winner["number"]} * {game_winner["unmarked_sum"]} = {answer}'
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/day_4_test.py
@@ -0,0 +1,135 @@
+from advent_of_code_2021.day_4 import Board, parse_data, run_game
+
+TEST_DATA = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7"""
+TEST_DATA = [item.strip() for item in TEST_DATA.split("\n")]
+
+EXPECTED_TEST_RESULT = {
+    "numbers": [
+        7,
+        4,
+        9,
+        5,
+        11,
+        17,
+        23,
+        2,
+        0,
+        14,
+        21,
+        24,
+        10,
+        16,
+        13,
+        6,
+        15,
+        25,
+        12,
+        22,
+        18,
+        20,
+        8,
+        19,
+        3,
+        26,
+        1,
+    ],
+    "boards": [
+        [
+            [22, 13, 17, 11, 0],
+            [8, 2, 23, 4, 24],
+            [21, 9, 14, 16, 7],
+            [6, 10, 3, 18, 5],
+            [1, 12, 20, 15, 19],
+        ],
+        [
+            [3, 15, 0, 2, 22],
+            [9, 18, 13, 17, 5],
+            [19, 8, 7, 25, 23],
+            [20, 11, 10, 24, 4],
+            [14, 21, 16, 12, 6],
+        ],
+        [
+            [14, 21, 17, 24, 4],
+            [10, 16, 15, 9, 19],
+            [18, 8, 23, 26, 20],
+            [22, 11, 13, 6, 5],
+            [2, 0, 12, 3, 7],
+        ],
+    ],
+}
+
+
+def test_parse_data():
+    result = parse_data(TEST_DATA)
+    assert result["numbers"] == EXPECTED_TEST_RESULT["numbers"]
+    assert len(result["boards"]) == len(EXPECTED_TEST_RESULT["boards"])
+    for result_board, expected_board in zip(
+        result["boards"], EXPECTED_TEST_RESULT["boards"]
+    ):
+        assert result_board == expected_board
+    assert result == EXPECTED_TEST_RESULT
+
+
+def test_board_init():
+    result = parse_data(TEST_DATA)
+    board = Board(parse_data(TEST_DATA)["boards"][0])
+    import pprint
+
+    pprint.pprint(result["boards"][0])
+    pprint.pprint(board.board)
+
+
+def test_board_pick_number():
+    board = Board(parse_data(TEST_DATA)["boards"][0])
+    assert board.board[(0, 0)]["picked"] is False
+    board.pick_number(22)
+    assert board.board[(0, 0)]["picked"] is True
+
+
+def test_board_is_winner():
+    result = parse_data(TEST_DATA)
+    board = Board(parse_data(TEST_DATA)["boards"][2])
+    assert board.is_winner is False
+    for number in result["numbers"]:
+        board.pick_number(number)
+        if number != 24:
+            assert board.is_winner is False
+        else:
+            assert board.is_winner is True
+            break
+
+
+def test_board_unmarked_sum():
+    result = parse_data(TEST_DATA)
+    board = Board(parse_data(TEST_DATA)["boards"][2])
+    for number in result["numbers"]:
+        board.pick_number(number)
+        if number == 24:
+            assert board.is_winner is True
+            assert board.unmarked_sum == 188
+            break
+
+
+def test_run_game():
+    parsed_data = parse_data(TEST_DATA)
+    game_winner = run_game(parsed_data)
+    assert game_winner["number"] == 24
+    assert game_winner["unmarked_sum"] == 188
